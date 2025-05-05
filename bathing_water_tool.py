151af9c5b71844c8ec9f9c11f485c59a4f49ff2a
mol-sarg <sarg.mol2@gmail.com>
2025-05-05 11:41:21 +0100

    Update bathing_water_tool.py

--- bathing_water_tool.py
+++ bathing_water_tool.py
@@ -1,3 +1,7 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
 st.set_page_config(page_title="Bathing Water Quality Explorer", layout="wide")
 
 st.title("🌊 Bathing Water Quality Explorer")
